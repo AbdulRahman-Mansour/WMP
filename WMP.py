7e14055c837ec694eec53f0265b5ca4e110cf6bc
AbdulRahman-Mansour <abdulrahman.masloub@gmail.com>
2025-08-24 07:15:02 +0300

    Update WMP.py

--- WMP.py
+++ WMP.py
@@ -167,7 +167,7 @@ class ERPNext:
                     "_user_tags", "_comments", "_assign", "_liked_by",
                     # "_see",
                 ]),
-                "filters": '[["is_cancelled", "!=", 1]]',
+                "filters": '[["status", "!=", "Cancelled"]]',
                 "limit_page_length": "None"
             }
         },
